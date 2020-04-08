f988df3492498b525eff80c5378f4f56a0cd081c
Andre Lazari <andre.lazari@arquivei.com.br>
2020-04-08 14:59:33 -0300

    Add health queue execution to prevent infinite queue growing

--- src/Checkers/QueueChecker.php
+++ src/Checkers/QueueChecker.php
@@ -2,6 +2,12 @@
 
 namespace Laravel\Health\Checkers;
 
+use App\Exceptions\Handler;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Queue\QueueManager;
+use Illuminate\Queue\QueueServiceProvider;
 use Illuminate\Queue\Worker;
 use Illuminate\Queue\WorkerOptions;
 use Illuminate\Support\Facades\Cache;
@@ -9,24 +15,19 @@ use Laravel\Health\Jobs\QueueJob;
 
 class QueueChecker extends BaseChecker
 {
-    public function check() : QueueChecker
+    public function check(): QueueChecker
     {
         try {
+            $queue = $this->resources['queue-name'] ?: 'health-check';
 
-            \Queue::pushOn($this->resources['queue-name'], new QueueJob());
+            \Queue::pushOn($queue, new QueueJob());
 
             $connection = $this->resources['connection'] ?: config('queue.default');
 
-            $queue = config("queue.connections.{$connection}.queue");
-
-            $worker = new Worker();
-
-            $worker->setCache('cache')->driver();
-
-            $q = $worker->runNextJob($connection, $queue, new WorkerOptions([0,0]));
+            \Artisan::call('queue:work', [$connection, '--queue' => $queue, '--once' => true]);
 
             $this->makeResponse(null, true);
-        } catch (\Exception $exception) {
+        } catch (\Throwable $exception) {
             $this->makeResponse($exception->getMessage(), false);
         } finally {
             return $this;
